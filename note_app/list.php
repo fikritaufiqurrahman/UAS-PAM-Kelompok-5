dceb3313ccdb2a4fd469561fbdd96ca2f469ebf0
fikri taufiqurrahman <78025605+fikritaufiqurrahman@users.noreply.github.com>
2022-12-28 21:10:57 +0700

    Delete note_app directory

--- note_app/list.php
+++
@@ -1,9 +0,0 @@
-    
-
-<?php 
-
-    $connection = new mysqli("localhost","id20061775_root","?b23{U1W=w<hnz-X","id20061775_note_app");
-    $data       = mysqli_query($connection, "select * from note_app");
-    $data       = mysqli_fetch_all($data, MYSQLI_ASSOC);
-
-    echo json_encode($data);
